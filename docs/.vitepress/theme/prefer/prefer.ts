69d31dcb2bb342f121e87f8a7c17df654106aa5b
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-11-22 15:21:10 +0800

    fix: do not use localStorage out browser

--- docs/.vitepress/theme/prefer/prefer.ts
+++ docs/.vitepress/theme/prefer/prefer.ts
@@ -1,4 +1,6 @@
 import {ref} from 'vue'
 
+export const inBrowser = typeof window !== 'undefined'
+
 export const preferMavenKey = 'command-api-docs-prefer-maven'
-export const preferMaven = ref((localStorage.getItem(preferMavenKey) ?? 'true') === 'true')
+export const preferMaven = ref(inBrowser ? (localStorage.getItem(preferMavenKey) ?? 'true') === 'true' : true)
