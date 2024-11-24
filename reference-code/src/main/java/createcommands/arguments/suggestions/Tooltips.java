cf301d159c53002fc91aa438bf06f7ce57930c56
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-11-24 14:08:45 +0800

    chore: add .editorconfig, uses 8 spaces for continuation indent

--- reference-code/src/main/java/createcommands/arguments/suggestions/Tooltips.java
+++ reference-code/src/main/java/createcommands/arguments/suggestions/Tooltips.java
@@ -5,7 +5,12 @@ import dev.jorel.commandapi.BukkitTooltip;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.IStringTooltip;
 import dev.jorel.commandapi.StringTooltip;
-import dev.jorel.commandapi.arguments.*;
+import dev.jorel.commandapi.arguments.Argument;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.LocationArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import dev.jorel.commandapi.arguments.SafeSuggestions;
+import dev.jorel.commandapi.arguments.StringArgument;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -21,40 +26,40 @@ class Tooltips {
         // #region stringSuggestionTooltipsExampleDeclare
         List<Argument<?>> arguments = new ArrayList<>();
         arguments.add(new StringArgument("emote")
-                .replaceSuggestions(ArgumentSuggestions.stringsWithTooltips(info ->
-                        new IStringTooltip[]{
-                                StringTooltip.ofString("wave", "Waves at a player"),
-                                StringTooltip.ofString("hug", "Gives a player a hug"),
-                                StringTooltip.ofString("glare", "Gives a player the death glare")
-                        }
-                ))
+            .replaceSuggestions(ArgumentSuggestions.stringsWithTooltips(info ->
+                new IStringTooltip[]{
+                    StringTooltip.ofString("wave", "Waves at a player"),
+                    StringTooltip.ofString("hug", "Gives a player a hug"),
+                    StringTooltip.ofString("glare", "Gives a player the death glare")
+                }
+            ))
         );
         arguments.add(new PlayerArgument("target"));
         // #endregion stringSuggestionTooltipsExampleDeclare
 
         // #region stringSuggestionTooltipsExampleRegister
         new CommandAPICommand("emote")
-                .withArguments(arguments)
-                .executesPlayer((player, args) -> {
-                    String emote = (String) args.get("emote");
-                    Player target = (Player) args.get("target");
+            .withArguments(arguments)
+            .executesPlayer((player, args) -> {
+                String emote = (String) args.get("emote");
+                Player target = (Player) args.get("target");
 
-                    switch (emote) {
-                        case "wave":
-                            target.sendMessage(player.getName() + " waves at you!");
-                            break;
-                        case "hug":
-                            target.sendMessage(player.getName() + " hugs you!");
-                            break;
-                        case "glare":
-                            target.sendMessage(player.getName() + " gives you the death glare...");
-                            break;
-                        default:
-                            player.sendMessage("Invalid emote '" + emote + "'!");
-                            break;
-                    }
-                })
-                .register();
+                switch (emote) {
+                    case "wave":
+                        target.sendMessage(player.getName() + " waves at you!");
+                        break;
+                    case "hug":
+                        target.sendMessage(player.getName() + " hugs you!");
+                        break;
+                    case "glare":
+                        target.sendMessage(player.getName() + " gives you the death glare...");
+                        break;
+                    default:
+                        player.sendMessage("Invalid emote '" + emote + "'!");
+                        break;
+                }
+            })
+            .register();
         // #endregion stringSuggestionTooltipsExampleRegister
     }
 
@@ -98,24 +103,24 @@ class Tooltips {
     {
         // #region customTooltipExampleRegister
         CustomItem[] customItems = new CustomItem[]{
-                new CustomItem(new ItemStack(Material.DIAMOND_SWORD), "God sword", "A sword from the heavens"),
-                new CustomItem(new ItemStack(Material.PUMPKIN_PIE), "Sweet pie", "Just like grandma used to make")
+            new CustomItem(new ItemStack(Material.DIAMOND_SWORD), "God sword", "A sword from the heavens"),
+            new CustomItem(new ItemStack(Material.PUMPKIN_PIE), "Sweet pie", "Just like grandma used to make")
         };
 
         new CommandAPICommand("giveitem")
-                .withArguments(new StringArgument("item").replaceSuggestions(ArgumentSuggestions.stringsWithTooltips(customItems))) // We use customItems[] as the input for our suggestions with tooltips
-                .executesPlayer((player, args) -> {
-                    String itemName = (String) args.get("item");
+            .withArguments(new StringArgument("item").replaceSuggestions(ArgumentSuggestions.stringsWithTooltips(customItems))) // We use customItems[] as the input for our suggestions with tooltips
+            .executesPlayer((player, args) -> {
+                String itemName = (String) args.get("item");
 
-                    // Give them the item
-                    for (CustomItem item : customItems) {
-                        if (item.getName().equals(itemName)) {
-                            player.getInventory().addItem(item.getItem());
-                            break;
-                        }
+                // Give them the item
+                for (CustomItem item : customItems) {
+                    if (item.getName().equals(itemName)) {
+                        player.getInventory().addItem(item.getItem());
+                        break;
                     }
-                })
-                .register();
+                }
+            })
+            .register();
         // #endregion customTooltipExampleRegister
     }
 
@@ -123,24 +128,24 @@ class Tooltips {
         // #region tooltipsWithSafeSuggestionsExampleDeclare
         List<Argument<?>> arguments = new ArrayList<>();
         arguments.add(new LocationArgument("location")
-                .replaceSafeSuggestions(SafeSuggestions.tooltips(info -> {
-                    // We know the sender is a player if we use .executesPlayer()
-                    Player player = (Player) info.sender();
-                    return BukkitTooltip.arrayOf(
-                            BukkitTooltip.ofString(player.getWorld().getSpawnLocation(), "World spawn"),
-                            BukkitTooltip.ofString(player.getBedSpawnLocation(), "Your bed"),
-                            BukkitTooltip.ofString(player.getTargetBlockExact(256).getLocation(), "Target block")
-                    );
-                })));
+            .replaceSafeSuggestions(SafeSuggestions.tooltips(info -> {
+                // We know the sender is a player if we use .executesPlayer()
+                Player player = (Player) info.sender();
+                return BukkitTooltip.arrayOf(
+                    BukkitTooltip.ofString(player.getWorld().getSpawnLocation(), "World spawn"),
+                    BukkitTooltip.ofString(player.getBedSpawnLocation(), "Your bed"),
+                    BukkitTooltip.ofString(player.getTargetBlockExact(256).getLocation(), "Target block")
+                );
+            })));
         // #endregion tooltipsWithSafeSuggestionsExampleDeclare
 
         // #region tooltipsWithSafeSuggestionsExampleRegister
         new CommandAPICommand("warp")
-                .withArguments(arguments)
-                .executesPlayer((player, args) -> {
-                    player.teleport((Location) args.get("location"));
-                })
-                .register();
+            .withArguments(arguments)
+            .executesPlayer((player, args) -> {
+                player.teleport((Location) args.get("location"));
+            })
+            .register();
         // #endregion tooltipsWithSafeSuggestionsExampleRegister
     }
 }
