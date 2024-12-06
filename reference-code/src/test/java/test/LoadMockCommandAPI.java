c95caab27828db571fff848a8531939657516b3c
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-12-06 21:21:32 +0800

    feat: add cache to maven in build action

--- reference-code/src/test/java/test/LoadMockCommandAPI.java
+++ reference-code/src/test/java/test/LoadMockCommandAPI.java
@@ -10,7 +10,6 @@ import dev.jorel.commandapi.MockCommandAPIPlugin;
 import org.junit.jupiter.api.BeforeEach;
 
 class LoadMockCommandAPI {
-    test
     // #region loadMockCommandAPIExample
     @BeforeEach
     public void setUp() {
