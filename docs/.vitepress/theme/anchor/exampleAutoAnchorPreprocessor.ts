6fd4960372c3427be35d150064168a2a3b120c50
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-12-07 09:09:23 +0800

    feat: improve example block anchor

--- docs/.vitepress/theme/anchor/exampleAutoAnchorPreprocessor.ts
+++ docs/.vitepress/theme/anchor/exampleAutoAnchorPreprocessor.ts
@@ -7,9 +7,12 @@ export const exampleAutoAnchorPreprocessor = (md: MarkdownIt) => {
         const processedLines = lines.map((line) => {
             if (line.startsWith(":::") && (line.includes(":::tip Example") || line.includes("::: tip Example"))) {
                 const title = line.split(/tip (.+)/)[1]
+                const processedTitle = slugify(title.replace(" – ", ""));
                 return `
-<div style="height: 0; color: rgba(0,0,0,0%);position: relative;top: 50px">
-<h3 id="${slugify(title.replace(" – ", ""))}">${title}</h3>                
+<div style="height: 0; color: rgba(0,0,0,0%);position: relative;top: 32px">
+    <h3 id="${processedTitle}">${title}
+        <a class="header-anchor" href="#${processedTitle}" aria-label="Permalink to &quot;${title}&quot;">​</a>
+    </h3>                
 </div>
 
 ${line}
