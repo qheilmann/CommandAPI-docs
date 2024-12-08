6ddaa701ce4c3890e0eba983e94efcceb7689531
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-12-08 19:27:32 +0800

    fix: fix title anchor preprocessor

--- docs/.vitepress/theme/anchor/titleAnchorPreprocessor.ts
+++ docs/.vitepress/theme/anchor/titleAnchorPreprocessor.ts
@@ -4,7 +4,14 @@ import slugifyTitle from "./slugify";
 export const titleAnchorPreprocessor = (md: MarkdownIt) => {
     md.core.ruler.before('normalize', 'title-anchor', (state) => {
         const lines = state.src.split("\n");
+        let inCodeBlock = false;
         const processedLines = lines.map((line) => {
+            if (line.startsWith("```")) {
+                inCodeBlock = !inCodeBlock;
+            }
+            if (inCodeBlock) {
+                return line;
+            }
             if (line.startsWith("#")) {
                 const title = line.replace(new RegExp("#", "g"), "");
                 const processedTitle = (line.includes("{") && line.includes("}")) ?
